8f91260e014c2c8fec9ffef2ffa1131dff104667
Anusha Yerra <ianushayerra.ca@gmail.com>
2026-09-23 20:33:00 -0400

    Implement Page Object Model login framework

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,32 +1,30 @@
 package tests;
+import base.BaseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.LoginPage;
+import org.testng.Assert;
 
 
-public class LoginTest {
 
-        WebDriver driver;
+public class LoginTest extends BaseTest {
 
-        @BeforeMethod
-        public void setUp() {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-        }
+    @Test
+    public void validLoginTest() {
 
-        @Test
-        public void openWebsite() {
-            driver.get("https://www.saucedemo.com/");
+        LoginPage loginPage = new LoginPage(driver);
 
-            System.out.println("Page title: " + driver.getTitle());
-        }
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLogin();
 
-        @AfterMethod
-        public void tearDown() {
-            driver.quit();
-        }
+        Assert.assertTrue(
+                driver.getCurrentUrl().contains("inventory"),
+                "Login was not successful"
+        );
     }
+}
